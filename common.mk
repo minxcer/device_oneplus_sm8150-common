33a0e80c9d2583bb37af61f19b4d2689ca8f0d1d
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-13 14:50:19 +0000

    sm8150-common: Switch to stock sensors HAL
    Change-Id: I05fbd05909c44b42ccf8635a27c1f29359cd99bd
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- common.mk
+++ common.mk
@@ -400,8 +400,6 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl.oneplus_msmnile \
-    android.hardware.sensors@1.0-service.oneplus_msmnile \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
